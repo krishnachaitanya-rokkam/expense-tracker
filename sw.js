77cc67f15747d5685223173eb14d0678638623e2
krishnachaitanya-rokkam <krishnachaitanya.rokkam@outlook.com>
2026-09-02 21:46:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="expense-tracker-v13-signedout-final";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./favicon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
